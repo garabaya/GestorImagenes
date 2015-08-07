096720a6295e83f802e1321b7fd04eb331bee10e
Ruben Garabaya <garabaya@jig.es>
2015-08-07 14:33:53 +0200

    Migraciones

--- database/migrations/2015_08_07_121719_CrearTablaAlbumes.php
+++ database/migrations/2015_08_07_121719_CrearTablaAlbumes.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CrearTablaAlbumes extends Migration
 {
     /**
      * Run the migrations.
@@ -12,14 +12,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('usuarios', function (Blueprint $table) {
+        Schema::create('albumes', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nombre');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->string('pregunta');
-            $table->string('respuesta');
-            $table->rememberToken();
+            $table->string('descripcion');
+            $table->integer('usuario_id')->unsigned();
+            $table->foreign('usuario_id')->references('id')->on('usuarios');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('usuarios');
+        Schema::drop('albumes');
     }
 }
